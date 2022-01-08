72c454c6fdd5bc0458c95e3166ad6237f031f9f9
Andrey <amigin@gmail.com>
2022-01-08 10:27:58 +0200

    added HttpContext

---
+++ src/http_ctx.rs
@@ -0,0 +1,90 @@
+use crate::{HttpFailResult, QueryString, WebContentType};
+use hyper::{Body, Method, Request};
+
+pub struct HttpContext {
+    req: Request<Body>,
+    path: String,
+}
+
+impl HttpContext {
+    pub fn new(req: Request<Body>) -> Self {
+        let path = req.uri().path().to_lowercase();
+        Self { req, path }
+    }
+
+    pub fn get_required_header(&self, header_name: &str) -> Result<&str, HttpFailResult> {
+        for (name, value) in self.req.headers() {
+            if name.as_str() == header_name {
+                return Ok(value.to_str().unwrap());
+            }
+        }
+
+        return Err(HttpFailResult::as_header_parameter_required(
+            format!("Header: {}", header_name).as_str(),
+        ));
+    }
+
+    pub async fn get_form_data(self) -> Result<QueryString, HttpFailResult> {
+        let body = self.req.into_body();
+        let full_body = hyper::body::to_bytes(body).await.unwrap();
+
+        let result = full_body.iter().cloned().collect::<Vec<u8>>();
+        let a = String::from_utf8(result).unwrap();
+
+        match QueryString::new(a.as_str()) {
+            Ok(result) => return Ok(result),
+            Err(err) => {
+                let result = HttpFailResult {
+                    metric_it: true,
+                    content: format!("Can not parse Form Data. {:?}", err).into_bytes(),
+                    content_type: WebContentType::Text,
+                    status_code: 412,
+                };
+
+                return Err(result);
+            }
+        }
+    }
+
+    pub fn get_method(&self) -> &Method {
+        self.req.method()
+    }
+
+    pub fn get_path(&self) -> String {
+        self.path.as_str().to_lowercase()
+    }
+
+    pub fn get_query_string(&self) -> Result<QueryString, HttpFailResult> {
+        let query = self.req.uri().query();
+
+        match query {
+            Some(query) => Ok(QueryString::new(query)?),
+            None => Err(HttpFailResult::as_forbidden(Some(
+                "No query string found".to_string(),
+            ))),
+        }
+    }
+
+    pub fn get_host(&self) -> &str {
+        std::str::from_utf8(&self.req.headers().get("host").unwrap().as_bytes()).unwrap()
+    }
+
+    pub fn get_scheme(&self) -> String {
+        let headers = self.req.headers();
+        let proto_header = headers.get("X-Forwarded-Proto");
+
+        if let Some(scheme) = proto_header {
+            let bytes = scheme.as_bytes();
+            return String::from_utf8(bytes.to_vec()).unwrap();
+        }
+
+        let scheme = self.req.uri().scheme();
+
+        match scheme {
+            Some(scheme) => {
+                return scheme.to_string();
+            }
+            None => "http".to_string(),
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod http_ctx;
 mod http_fail_result;
 mod http_headers;
 mod query_string;
@@ -6,6 +7,7 @@ mod url_decoder_encoder;
 mod url_utils;
 mod web_content_type;
 
+pub use http_ctx::HttpContext;
 pub use http_fail_result::HttpFailResult;
 pub use http_headers::HttpHeaders;
 pub use query_string::QueryString;
