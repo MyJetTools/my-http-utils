07203dd59fcd1097104640f013860c0dcee3d25b
Andrey <amigin@gmail.com>
2022-01-08 12:18:54 +0200

    Added HttpOkResult

---
+++ src/http_ok_result.rs
@@ -0,0 +1,88 @@
+use crate::{HttpFailResult, WebContentType};
+use hyper::{Body, Response, StatusCode};
+use serde::Serialize;
+
+#[derive(Clone)]
+pub enum HttpOkResult {
+    Ok,
+
+    Content {
+        content_type: Option<WebContentType>,
+        content: Vec<u8>,
+    },
+    Text {
+        text: String,
+    },
+
+    Redirect {
+        url: String,
+    },
+}
+
+impl HttpOkResult {
+    pub fn create_json_response<T: Serialize>(model: T) -> Result<HttpOkResult, HttpFailResult> {
+        let json = serde_json::to_vec(&model).unwrap();
+        Ok(HttpOkResult::Content {
+            content_type: Some(WebContentType::Json),
+            content: json,
+        })
+    }
+
+    pub fn create_as_usize(number: usize) -> Result<HttpOkResult, HttpFailResult> {
+        Ok(HttpOkResult::Content {
+            content_type: Some(WebContentType::Text),
+            content: number.to_string().into_bytes(),
+        })
+    }
+
+    pub fn redirect(src: &str) -> Result<HttpOkResult, HttpFailResult> {
+        Ok(HttpOkResult::Redirect {
+            url: src.to_string(),
+        })
+    }
+}
+
+impl Into<HttpOkResult> for String {
+    fn into(self) -> HttpOkResult {
+        HttpOkResult::Content {
+            content_type: Some(WebContentType::Text),
+            content: self.into_bytes(),
+        }
+    }
+}
+
+impl Into<Response<Body>> for HttpOkResult {
+    fn into(self) -> Response<Body> {
+        return match self {
+            HttpOkResult::Ok => Response::builder()
+                .header("Content-Type", WebContentType::Text.to_string())
+                .status(200)
+                .body(Body::from("OK"))
+                .unwrap(),
+            HttpOkResult::Content {
+                content_type,
+                content,
+            } => match content_type {
+                Some(content_type) => Response::builder()
+                    .header("Content-Type", content_type.to_string())
+                    .status(200)
+                    .body(Body::from(content))
+                    .unwrap(),
+                None => Response::builder()
+                    .status(200)
+                    .body(Body::from(content))
+                    .unwrap(),
+            },
+            HttpOkResult::Text { text } => Response::builder()
+                .header("Content-Type", WebContentType::Text.to_string())
+                .status(200)
+                .body(Body::from(text))
+                .unwrap(),
+            HttpOkResult::Redirect { url } => Response::builder()
+                .status(StatusCode::PERMANENT_REDIRECT)
+                .header("Location", url)
+                .body(Body::empty())
+                .unwrap(),
+        };
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod http_ctx;
 mod http_fail_result;
 mod http_headers;
+mod http_ok_result;
 mod query_string;
 mod request_ip;
 pub mod url_decoder;
@@ -11,6 +12,7 @@ mod web_content_type;
 pub use http_ctx::HttpContext;
 pub use http_fail_result::HttpFailResult;
 pub use http_headers::HttpHeaders;
+pub use http_ok_result::HttpOkResult;
 pub use query_string::QueryString;
 pub use request_ip::RequestIp;
 pub use web_content_type::WebContentType;
